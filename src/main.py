08e23cf7725fcfeb6cee2927f21aa0057cbaba5e
mikhail izotov <157229582+mikkoBB@users.noreply.github.com>
2024-02-09 16:03:13 +0300

    prodram_work

--- src/main.py
+++ src/main.py
@@ -1,17 +1,18 @@
 import json
 from datetime import datetime
-from utils import sorted_operations
+from utils import get_sorted_operations
+from utils import print_date
+from utils import get_sent
 
 
 def main():
     print("Вывести 5 последних выполненных операций, нажать 'Enter'")
     user_input = input()
-    last_transactions = get_sorted_operations()
-    x = transaction
-    for x in last_transactions:
-        print(print_date(x.get('date')), x.get('description'))
-        print(f"{get_sent(x.get('from'))} -> {get_sent(x.get('to'))}")
-        print(x.get('operationAmount').get('amount'), x.get('operationAmount').get('currency').get('name'))
+    x = get_sorted_operations()
+    for operation in x:
+        print(print_date(operation.get('date')), operation.get('description'))
+        print(f"{get_sent(operation.get('from'))} -> {get_sent(operation.get('to'))}")
+        print(operation.get('operationAmount').get('amount'),operation.get('operationAmount').get('currency').get('name'))
         print()
 
 
