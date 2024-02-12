bdf1499b56c270da8b21ca00c15ac6cff3e0885c
mikhail izotov <157229582+mikkoBB@users.noreply.github.com>
2024-02-12 14:52:05 +0300

    main 2

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 import json
 from datetime import datetime
+from src import utils
 from utils import get_sorted_operations
 from utils import print_date
 from utils import get_sent
-
 
 def main():
     print("Вывести 5 последних выполненных операций, нажать 'Enter'")
@@ -12,9 +12,11 @@ def main():
     for operation in x:
         print(print_date(operation.get('date')), operation.get('description'))
         print(f"{get_sent(operation.get('from'))} -> {get_sent(operation.get('to'))}")
-        print(operation.get('operationAmount').get('amount'),operation.get('operationAmount').get('currency').get('name'))
+        print(operation.get('operationAmount').get('amount'), operation.get('operationAmount').get('currency').get('name'))
         print()
 
 
 if __name__ == '__main__':
     main()
+
+
